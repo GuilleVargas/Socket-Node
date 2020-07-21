bea7a3a01996d45c9cdfe527af9c8b096a293233
GuilleVargas <guillevargas10@gmail.com>
2020-07-21 13:27:09 +0200

    Added socket-node

---
+++ app.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const http = require ('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+
+const publicPath = path.join (__dirname, '/public');
+const PORT = process.env.PORT || 4000;
+const app = express();
+app.use(express.static(publicPath));
+
+
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection',(socket)=>{
+    
+    console.log("Nuevo usuario conectado");
+
+    socket.on('clientNewMessage',(msg)=>console.log(msg));
+
+    socket.emit('serverNewMessage',{
+    from: 'servidor',
+    text:'Es una prueba',
+    createdAt:192140921
+    });
+
+    socket.emit('serverNewMessage',{
+        from: 'servidor',
+        text:'Segunda prueba',
+        createdAt:192140921
+        });
+});
+
+
+
+
+server.listen(PORT,()=>console.log(`ServiDor escuchando en el puerto ${PORT}` ));
